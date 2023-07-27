f3987620ee56468d5fdc23fda00244522c55f1de
Nihal SANDHU <121277127+nihalsandhu09@users.noreply.github.com>
2023-07-27 07:16:48 +0000

    operatorsi= j in ja vcascript

--- 01_basics/03_convcersionoperatoins.js
+++ 01_basics/03_convcersionoperatoins.js
@@ -27,7 +27,57 @@ console.log(booleanIsLoggedIn)
 //" hitesh" => true
 
 
-let someNumber = 33 
-let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log( typeof stringNumber)
+// let someNumber = 33 
+// let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log( typeof stringNumber)
+
+// ************************************** operations **********************
+
+// let value = 3 
+// let negValue =  -value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%2);
+
+// let str1 = "hello"
+// let str2 = "nihal"
+// let str3 = str1+ str2 
+// console.log(str3)
+
+// console.log("1"+ 2)
+// console.log(1+ "2")
+// console.log("1"+ 2+2)
+// console.log(1+2+"2")
+
+
+
+
+// console.log(+true)
+// console.log(+"")
+
+
+
+// let num1,num2,num3
+
+// num1 = num2 = num3=2+2     // code should be readble not like this  
+
+// let gameCounter = 100
+// // gameCounter++;    postfix
+// ++gameCounter;      // prefix 
+// console.log(gameCounter)
+
+// postfix  
+ let a = 3
+ let b = a++
+ console.log(`a = ${a} b => ${b}`)
+ 
+//  prefix 
+let x = 3
+let y = ++x
+console.log(`x = ${x} y =${y}`)
