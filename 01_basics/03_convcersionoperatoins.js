8d5887b3384b57b316e55724564e4a60d707c835
Nihal SANDHU <121277127+nihalsandhu09@users.noreply.github.com>
2023-07-27 06:06:55 +0000

    CVonvcersion of data types

---
+++ 01_basics/03_convcersionoperatoins.js
@@ -0,0 +1,33 @@
+// convcersion of data types  
+
+// let score =" 33abc "  
+// let score = null 
+// let score = undefined 
+// let score = true
+// let score = "hitesh"
+// console.log(typeof score)
+// console.log(typeof  (score))
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log( valueInNumber)
+
+
+
+//'333' => 33 
+// '33abc' => NaN 
+// true = 1; false => 0
+
+let isLOggedIn = ""
+let booleanIsLoggedIn = Boolean(isLOggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 =. true; 0 => false
+//" "=> false
+//" hitesh" => true
+
+
+let someNumber = 33 
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log( typeof stringNumber)
