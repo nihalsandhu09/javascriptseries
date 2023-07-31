6940dfbbd08b9f4cab3e60d899d51fc1e6a50f8b
Nihal SANDHU <121277127+nihalsandhu09@users.noreply.github.com>
2023-07-31 01:53:15 +0000

    depth in objects continues

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,62 @@
+//  here will se how can we crete object with constructor 
+
+// const tinderUser = new Object()
+ const tinderUser = {}
+ tinderUser.id = "123abc"
+ tinderUser.name= "Sam"
+ tinderUser.isLoggedIn =false
+// console.log(tinderUser)
+
+
+const regularUser = {
+email : "Sum@gmail.com",
+fullname:{
+userfullname:{
+    firstname:"hitesh",
+    lastname:"choudhary"
+}
+}
+}
+
+// console.log(regularUser.fullname?.userfullname.firstname) 
+const obj1 ={
+    1:"a",
+    2:"b"
+}
+const obj2 ={
+    3:"a",
+    4:"b"
+}
+const obj3 = Object.assign(obj1, obj2)  //  assign method 
+// const newObj = {...obj1,...obj2}  using spread operator 
+// console.log(obj3)
+// console.log(newObj)
+
+const users = [
+    {
+        id:12,
+
+    },
+    {
+        id:13
+    }
+]
+
+users[1].id = 14;    // this is how you can access array of objects properties  
+// console.log(users)
+
+console.log(tinderUser);
+
+let lops = Object.keys(tinderUser).map((e)=>{
+    return e 
+})
+let lops1 = Object.values(tinderUser).map((e)=>{
+    return e 
+})
+console.log(lops)
+console.log(lops1)
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
