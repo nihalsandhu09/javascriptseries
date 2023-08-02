692717322002b7995494f2f5219cd739ea4df6b9
Nihal SANDHU <121277127+nihalsandhu09@users.noreply.github.com>
2023-08-02 02:24:25 +0000

    Scope block and global

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,20 @@
+//  lets understand scope 
+// global scope and local scope
+
+ 
+
+// global scope 
+
+{
+    // scope block scope 
+    let a = 10
+const b = 20
+var c = 30
+}
+
+console.log(a)       // a is not de3fined 
+console.log(b)          // b is not defined
+console.log(c)        //  print 30 
+
+
+  
