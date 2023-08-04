8440afcb3ec9c104f8dc744ca5fd30776911f849
Nihal SANDHU <121277127+nihalsandhu09@users.noreply.github.com>
2023-08-04 11:16:11 +0000

    do while loops

---
+++ 05_iterations/two.js
@@ -0,0 +1,26 @@
+
+// let index = 0;
+// while (index <=10) {
+//     console.log(`value of index is ${index}`)
+//     index = index+2
+// }
+
+// let myArray = ['flash','batman','superman']
+// let arr =0 
+// while(arr< myArray.length){
+//     console.log(`value is ${myArray[arr]}`)
+//      arr = arr + 1
+// }
+
+// let score = 11
+// do {
+//  console.log(`score is ${score}`)   
+//  score++
+// } while (score <=10);
+
+ let s = 0;
+do{
+    console.log("d9o this one time")
+    console.log(s)
+    s++
+}while(s <= 4)
