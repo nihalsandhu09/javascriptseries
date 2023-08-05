8ad24d86110cde4e02022593e6a1cad9d875d6b5
Nihal SANDHU <121277127+nihalsandhu09@users.noreply.github.com>
2023-08-05 05:02:15 +0000

    for in

---
+++ 05_iterations/four.js
@@ -0,0 +1,18 @@
+const myObject = {
+    js: 'javascript',
+    cpp:'c++',
+    rb:'ruby',
+swift : "swift by apple"
+}
+
+// for (const key in myObject) {
+//    console.log(` ${key} shortcut is for ${myObject[key]}`)
+// }
+
+// const programming = ["js","rb","py","java","cpp"]
+// for (const key in programming) {
+//     console.log(programming[key])
+// }
+
+
+
